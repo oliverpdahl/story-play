793651222845452e3ff71bf6a52f32238eca8ec2
oliverpdahl <oliverpdahl@gmail.com>
2020-11-07 09:42:47 -0700

    button from careers page

--- src/components/CareersPage/CareersPage.js
+++ src/components/CareersPage/CareersPage.js
@@ -3,16 +3,16 @@ import lion from "../../images/lion.png";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import CareerButton from "./CareerButton";
-import { Images } from "../../data/Images";
+import { Careers } from "../../data/Careers";
 import FlaticonAttribution from "../FlaticonAttribution";
 
 export default function CareersPage() {
   return (
     <div className="px-5">
       <CareerButton
-        to="/explore"
-        title="This Is A Job"
-        image={Images.lion.image}
+        to={Careers.zoologist.slug}
+        title={Careers.zoologist.title}
+        image={Careers.zoologist.buttonImg}
       />
       <FlaticonAttribution />
     </div>
---
+++ src/data/Careers.js
@@ -0,0 +1,19 @@
+import { Images } from "./Images";
+
+export const Careers = {
+  zoologist: {
+    title: "Zoologist",
+    buttonImg: Images.lion.image,
+    slug: "/explore",
+  },
+  marineBiologist: {
+    title: "Marine Biologist",
+    buttonImg: Images.lion.image,
+    slug: "/explore",
+  },
+  archaeologist: {
+    title: "Archaeologist",
+    buttonImg: Images.lion.image,
+    slug: "/explore",
+  },
+};
