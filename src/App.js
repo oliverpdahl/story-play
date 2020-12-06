f5398847eff52c064b060e7a8a63dfbfdd64f528
oliverpdahl <oliverpdahl@gmail.com>
2020-12-06 18:29:32 -0500

    Count loads with 0

--- src/App.js
+++ src/App.js
@@ -6,29 +6,32 @@ import CareersPage from "./components/CareersPage/CareersPage";
 import ExplorationPage from "./components/ExplorationPage/ExplorationPage";
 import NavBar from "./components/Navbar/Navbar";
 import { Careers } from "./data/Careers";
+import { CounterProvider } from "./store/index";
 
 export default function App() {
   return (
-    <Router>
-      <NavBar />
-      <div>
-        {/* Route components are rendered if the path prop matches the current URL */}
-        <Route path="/careers">
-          <CareersPage />
-        </Route>
-        <Route
-          exact
-          path="/explore/:slug"
-          render={({ match }) => (
-            <ExplorationPage
-              career={Careers.find((c) => c.slug === match.params.slug)}
-            />
-          )}
-        ></Route>
-        <Route path="/" exact>
-          <LandingPage />
-        </Route>
-      </div>
-    </Router>
+    <CounterProvider>
+      <Router>
+        <NavBar />
+        <div>
+          {/* Route components are rendered if the path prop matches the current URL */}
+          <Route path="/careers">
+            <CareersPage />
+          </Route>
+          <Route
+            exact
+            path="/explore/:slug"
+            render={({ match }) => (
+              <ExplorationPage
+                career={Careers.find((c) => c.slug === match.params.slug)}
+              />
+            )}
+          ></Route>
+          <Route path="/" exact>
+            <LandingPage />
+          </Route>
+        </div>
+      </Router>
+    </CounterProvider>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,13 +1,17 @@
 import React from "react";
-import {CharacterImages} from "../data/Character";
-
+import { CharacterImages } from "../data/Character";
+import { useCounterStore } from "../store/index.js";
 export default function CharacterImage() {
+  const [count, setCount] = useCounterStore();
   return (
     <div className="CharacterImage">
-      <img src={CharacterImages.test_character.image} 
-      alt={CharacterImages.test_character.alternate} 
-      height="auto" 
-      width="70%" />
+      <p>You clicked {count} times</p>
+      <img
+        src={CharacterImages.test_character.image}
+        alt={CharacterImages.test_character.alternate}
+        height="auto"
+        width="70%"
+      />
     </div>
   );
 }
