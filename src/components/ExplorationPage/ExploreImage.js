145494149baa14c5ed22cd2f504485923810df8f
oliverpdahl <oliverpdahl@gmail.com>
2020-12-06 17:04:19 -0500

    audio plays

--- src/components/ExplorationPage/ExploreImage.js
+++ src/components/ExplorationPage/ExploreImage.js
@@ -1,27 +1,50 @@
-import React from "react";
+import React, { useRef } from "react";
 import PropTypes from "prop-types";
 import { Popover, OverlayTrigger } from "react-bootstrap";
+import soundfile from "../../audio/test.mp3";
+import { AudioPlayerProvider, useAudioPlayer } from "react-use-audio-player";
 
 function ExploreImage(props) {
-  const playAudio = () => {
-    props.audio.play();
-  };
+  const AudioPlayer = ({ file }) => {
+    const { togglePlayPause, ready, loading, playing } = useAudioPlayer({
+      src: file,
+      format: "mp3",
+      autoplay: false,
+      onend: () => console.log("sound has ended!"),
+    });
 
+    if (!ready && !loading) return <div>No audio to play</div>;
+    if (loading) return <div>Loading audio</div>;
+
+    return (
+      <div>
+        <button onClick={togglePlayPause}>{playing ? "Pause" : "Play"}</button>
+      </div>
+    );
+  };
+  const start = () => {};
   const popover = (
     <Popover id="popover-basic">
       <Popover.Title as="h3">{props.title}</Popover.Title>
       <Popover.Content>{props.content}</Popover.Content>
     </Popover>
   );
+  const audioRef = useRef();
   return (
-    <OverlayTrigger trigger="click" placement="bottom" overlay={popover}>
-      <img
-        src={props.image}
-        alt={props.alternate}
-        style={{ width: "100%", maxHeight: "100%" }}
-        onClick={playAudio}
-      />
-    </OverlayTrigger>
+    <div>
+      <AudioPlayerProvider>
+        <AudioPlayer file={soundfile} />
+      </AudioPlayerProvider>
+      <audio ref={audioRef} src="../../audio/test.mp3" controls autoPlay />
+      <OverlayTrigger trigger="click" placement="bottom" overlay={popover}>
+        <img
+          src={props.image}
+          alt={props.alternate}
+          style={{ width: "100%", maxHeight: "100%" }}
+          onClick={start}
+        />
+      </OverlayTrigger>
+    </div>
   );
 }
 
