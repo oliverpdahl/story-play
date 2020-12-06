a5a364c6ee8664fd2717fda5f936c8d725724228
oliverpdahl <oliverpdahl@gmail.com>
2020-12-06 17:29:22 -0500

    audio plays and pauses on click

--- src/components/ExplorationPage/ExploreImage.js
+++ src/components/ExplorationPage/ExploreImage.js
@@ -5,7 +5,13 @@ import soundfile from "../../audio/test.mp3";
 import { AudioPlayerProvider, useAudioPlayer } from "react-use-audio-player";
 
 function ExploreImage(props) {
-  const AudioPlayer = ({ file }) => {
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Title as="h3">{props.title}</Popover.Title>
+      <Popover.Content>{props.content}</Popover.Content>
+    </Popover>
+  );
+  const ImageWithAudio = ({ file }) => {
     const { togglePlayPause, ready, loading, playing } = useAudioPlayer({
       src: file,
       format: "mp3",
@@ -13,37 +19,22 @@ function ExploreImage(props) {
       onend: () => console.log("sound has ended!"),
     });
 
-    if (!ready && !loading) return <div>No audio to play</div>;
-    if (loading) return <div>Loading audio</div>;
-
     return (
-      <div>
-        <button onClick={togglePlayPause}>{playing ? "Pause" : "Play"}</button>
-      </div>
-    );
-  };
-  const start = () => {};
-  const popover = (
-    <Popover id="popover-basic">
-      <Popover.Title as="h3">{props.title}</Popover.Title>
-      <Popover.Content>{props.content}</Popover.Content>
-    </Popover>
-  );
-  const audioRef = useRef();
-  return (
-    <div>
-      <AudioPlayerProvider>
-        <AudioPlayer file={soundfile} />
-      </AudioPlayerProvider>
-      <audio ref={audioRef} src="../../audio/test.mp3" controls autoPlay />
       <OverlayTrigger trigger="click" placement="bottom" overlay={popover}>
         <img
           src={props.image}
           alt={props.alternate}
           style={{ width: "100%", maxHeight: "100%" }}
-          onClick={start}
+          onClick={togglePlayPause}
         />
       </OverlayTrigger>
+    );
+  };
+  return (
+    <div>
+      <AudioPlayerProvider>
+        <ImageWithAudio file={soundfile} />
+      </AudioPlayerProvider>
     </div>
   );
 }
