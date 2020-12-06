74a6e1217235d7d2f1fd57140306c60ff12c3a99
oliverpdahl <oliverpdahl@gmail.com>
2020-12-06 16:08:40 -0500

    add play audio function

--- src/components/CareersPage/CareerButton.js
+++ src/components/CareersPage/CareerButton.js
@@ -2,8 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
 
-
 function CareerButton(props) {
+  const playAudio = () => {
+    props.audio.play();
+  };
+
   return (
     <Link to={props.to}>
       <div className="py-3">
@@ -11,6 +14,7 @@ function CareerButton(props) {
           block
           style={{ height: "20vh", maxWidth: "650px" }}
           className="btn-info mx-auto"
+          onClick={playAudio}
         >
           <img src={props.image} style={{ height: "18vh" }} alt={props.title} />
         </Button>
