3de8f4f4d81e6f392333fe3e27d40d38a665583f
oliverpdahl <oliverpdahl@gmail.com>
2020-12-07 12:26:22 -0500

    Add stars create

--- src/components/CharacterPage/CharacterButton.js
+++ src/components/CharacterPage/CharacterButton.js
@@ -4,10 +4,16 @@ import { Button } from "react-bootstrap";
 import { useCharacterStore } from "../../store/characterContext";
 import { useCounterStore } from "../../store/counterContext.js";
 import { useLocation } from "react-router-dom";
+import { StarFill } from "react-bootstrap-icons";
 function CharacterButton() {
   const [character, setBacklink] = useCharacterStore();
   const [count] = useCounterStore();
   const location = useLocation();
+  const listStars = () => {
+    for (let i = 0; i < count; i++) {
+      <StarFill color="yellow" key={i} />;
+    }
+  };
   return (
     <Link to={"/character"}>
       <div className="py-3">
@@ -18,7 +24,7 @@ function CharacterButton() {
           onClick={() => setBacklink(location.pathname)}
         >
           <div style={{ maxWidth: "100%", height: "100%" }}>
-            <p>You have {count} stars</p>
+            <p>{Array(count).fill(<StarFill color="yellow" />)}</p>
             <img
               src={character.image}
               style={{ maxWidth: "100%", height: "70%" }}
