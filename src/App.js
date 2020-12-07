85069192a63e152b970c996c3efadfb70a02a72f
oliverpdahl <oliverpdahl@gmail.com>
2020-12-06 19:09:40 -0500

    character provider

--- src/App.js
+++ src/App.js
@@ -7,33 +7,36 @@ import ExplorationPage from "./components/ExplorationPage/ExplorationPage";
 import NavBar from "./components/Navbar/Navbar";
 import { Careers } from "./data/Careers";
 import { CounterProvider } from "./store/counterContext";
+import { CharacterProvider } from "./store/characterContext";
 import CharacterPage from "./components/CharacterPage";
 
 export default function App() {
   return (
     <CounterProvider>
-      <Router>
-        <NavBar />
-        <div>
-          {/* Route components are rendered if the path prop matches the current URL */}
-          <Route path="/careers">
-            <CareersPage />
-          </Route>
-          <Route path="/character" component={CharacterPage} exact />
-          <Route
-            exact
-            path="/explore/:slug"
-            render={({ match }) => (
-              <ExplorationPage
-                career={Careers.find((c) => c.slug === match.params.slug)}
-              />
-            )}
-          ></Route>
-          <Route path="/" exact>
-            <LandingPage />
-          </Route>
-        </div>
-      </Router>
+      <CharacterProvider>
+        <Router>
+          <NavBar />
+          <div>
+            {/* Route components are rendered if the path prop matches the current URL */}
+            <Route path="/careers">
+              <CareersPage />
+            </Route>
+            <Route path="/character" component={CharacterPage} exact />
+            <Route
+              exact
+              path="/explore/:slug"
+              render={({ match }) => (
+                <ExplorationPage
+                  career={Careers.find((c) => c.slug === match.params.slug)}
+                />
+              )}
+            ></Route>
+            <Route path="/" exact>
+              <LandingPage />
+            </Route>
+          </div>
+        </Router>
+      </CharacterProvider>
     </CounterProvider>
   );
 }
