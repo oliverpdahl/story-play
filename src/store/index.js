795060eadaa58506a444b4fec515005760b53b13
oliverpdahl <oliverpdahl@gmail.com>
2020-12-06 19:02:21 -0500

    add character to state

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,10 @@
 import React, { useState, createContext, useContext } from "react";
 export const CounterContext = createContext();
 export const CounterProvider = (props) => {
-  const [count, setCount] = useState(0);
+  const [count, setCount, character] = useState(0);
   return (
     <CounterContext.Provider
-      value={[count, setCount]}
+      value={[count, setCount, character]}
       {...props}
     ></CounterContext.Provider>
   );
