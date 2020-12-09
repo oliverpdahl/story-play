a000dfac29e150eaf53d52f66a674f66867a8aff
oliverpdahl <oliverpdahl@gmail.com>
2020-12-09 08:13:58 -0500

    Added audio to audio.js

--- src/data/Audios.js
+++ src/data/Audios.js
@@ -1,7 +1,29 @@
 import test from "../audio/test.mp3";
-import track from "../audio/Animal Track - Zoologist.m4a";
+import track from "../audio/track.m4a";
+import binocular from "../audio/binocular.m4a";
+import boat from "../audio/boat.m4a";
+import brush from "../audio/brush.m4a";
+import cave_painting from "../audio/cave_painting.m4a";
+import dolphin from "../audio/dolphin.m4a";
+import gps from "../audio/gps.m4a";
+import lion from "../audio/lion.m4a";
+import pottery from "../audio/pottery.m4a";
+import scuba from "../audio/scuba.m4a";
+import shell from "../audio/shell.m4a";
+import submarine from "../audio/submarine.m4a";
 
 export const Audios = {
   test: test,
   track: track,
+  binocular: binocular,
+  boat: boat,
+  brush: brush,
+  cave_painting: cave_painting,
+  dolphin: dolphin,
+  gps: gps,
+  lion: lion,
+  pottery: pottery,
+  scuba: scuba,
+  shell: shell,
+  submarine: submarine,
 };
